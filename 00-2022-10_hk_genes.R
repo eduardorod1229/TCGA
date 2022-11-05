c4d0237e467e40a83e004d9b002e5251f04b97b1
Eduardo Rodriguez <71841555+eduardorod1229@users.noreply.github.com>
2022-11-05 00:15:46 -0700

    normalization
    scripts for normalization of RE using house keeping genes (ALB, ACTB, B2M)

---
+++ 00-2022-10_hk_genes.R
@@ -0,0 +1,68 @@
+# Formating of housekeeping genes
+
+hk_sub_trnsc <-  read.csv("K:/Documents/TCGA/Data/featureCounts.ncbiRefSeq.subset.transcript.csv", header =  TRUE, skip = 1, sep = '\t')
+hk_exomes <- read.csv("K:/Documents/TCGA/Data/featureCounts.ncbiRefSeq.subset.exon.csv", header =  TRUE, skip = 1, sep = '\t')
+hk_transcripts <- read.csv("K:/Documents/TCGA/Data/featureCounts.ncbiRefSeq.transcript.csv", header =  TRUE, skip = 1, sep = '\t')
+
+#The code below is to change the names of the transcripts (no-subset) file
+column_names <- function(data_frame){
+
+
+  bam_names <-  c(names(data_frame)[endsWith(names(data_frame), 'bam')])
+  names(data_frame)[endsWith(names(data_frame), 'bam')] <-
+    sapply(strsplit(bam_names, "\\."), function(x)
+      paste0(x[c(12,13,14)], collapse = "."))
+
+  colnames(data_frame) <-gsub("\\.", "-",colnames(data_frame))
+
+  X_names <-  c(names(data_frame)[startsWith(names(data_frame), 'X')])
+  names(data_frame)[startsWith(names(data_frame), 'X')] <- gsub("X","",X_names)
+
+  print(names(data_frame))
+}
+
+colnames(hk_transcripts) <- column_names(hk_transcripts)
+colnames(hk_sub_trnsc) <- column_names(hk_sub_trnsc)
+colnames(hk_exomes) <- column_names(hk_exomes)
+
+rename_count_columns <- function(df){
+  count_columns <- names(df)[-c(1:6)]
+  names(df)[-c(1:6)] <-
+    sapply(strsplit(names(df)[-c(1:6)], "-"), function(x)
+    paste0(x[c(1,2,3)], collapse = "-"))
+  print(names(df))
+}
+
+colnames(hk_exomes) <- rename_count_columns(hk_exomes)
+colnames(hk_sub_trnsc) <- rename_count_columns(hk_sub_trnsc)
+
+
+saveRDS(hk_exomes, 'K:/Documents/TCGA/Data/hk_genes_exomes.rds')
+saveRDS(hk_sub_trnsc, 'K:/Documents/TCGA/Data/hk_genes_subtranscripts.rds')
+saveRDS(hk_exomes, 'K:/Documents/TCGA/Data/hk_genes_transcripts.rds')
+
+
+transpose_df <- function(data_frame){
+  index <- seq(1,length(colnames(data_frame)))
+  data_frame <- rbind(names(data_frame),data_frame)
+  names(data_frame) <- names(index)
+  data_frame <- t(data_frame)
+  data_frame <- data.frame(data_frame, row.name = index)
+  names(data_frame) <- data_frame[1,]
+  return(data_frame)
+  print(head(data_frame,15))
+}
+
+hk_transcripts_t <- transpose_df(hk_transcripts)
+hk_sub_trnsc_t <- transpose_df(hk_sub_trnsc)
+hk_exomes_t <- transpose_df(hk_exomes)
+
+hk_transcripts_count <- hk_transcripts_t[-c(1:6),c('Geneid','ALB','ACTB','B2M')]
+saveRDS(hk_transcripts_count, 'K:/Documents/TCGA/Data/hk_trns_count.rds')
+
+hk_subt_count <- hk_sub_trnsc_t[-c(1:6),c('Geneid','ALB','ACTB','B2M')]
+saveRDS(hk_subt_count, 'K:/Documents/TCGA/Data/hk_subtranscripts_count.rds')
+
+hk_exomes_count <- hk_exomes_t[-c(1:6),c('Geneid','ALB','ACTB','B2M')]
+saveRDS(hk_exomes_count, 'K:/Documents/TCGA/Data/hk_exomes_count.rds')
+
---
+++ 001-2022-10_REdata.R
@@ -0,0 +1,37 @@
+system("cmd.exe", input = paste('"K:\\Software\\R-4.1.2\\bin\\Rscript.exe" K:\\Documents\\TCGA\\Scripts\\00-2022-10_hk_genes.R'))
+library(DT)
+library(data.table)
+
+setwd("K:/Documents/TCGA")
+
+
+# Read in the transposome dataset
+transp <-  read.csv("K:/Documents/TCGA/count_matrix2.csv.gz" , header = TRUE)
+
+#Merge the raw data
+
+split_tcga_name <-  function(subject_name){
+  sp <- strsplit(subject_name,split = "-")[[1]]
+  case_id <-  paste("TCGA-",sp[1], "-", sp[2], sep = "")
+  tumor_code <-  paste(strsplit(sp[3], split="")[[1]][-3], collapse= "")
+  is_tumor_sample <-  ifelse(as.integer(tumor_code)<10, 1, 0)
+  return(c(case_id, is_tumor_sample))
+}
+
+
+transp[, c("case_submitter_id_2","is_tumor")] <-  t(sapply(transp$subject, split_tcga_name))
+
+
+transp_backup <- transp
+
+transp <-  transp_backup
+tumor <-  1 # 1 to look at tumor, 0 to look at blood samples
+transp$is_tumor <- as.numeric(transp$is_tumor)
+saveRDS(transp, 'K:/Documents/TCGA/Data/transp.rds')
+
+transp <-  transp[which(transp$is_tumor==tumor), ]
+
+datatable(transp[,1:15],options <-  list(pageLength = 20))
+
+
+saveRDS(transp, 'K:/Documents/TCGA/Data/transp_tumor.rds')
